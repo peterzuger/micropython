86fe2dfce695f1a4ae8ee589f17445bc907bc705
Oliver Joos <oliver.joos@hispeed.ch>
2023-03-07 00:23:04 +0100

    stm32/boards: Add support for the NUCLEO_H753ZI board.

--- ports/stm32/powerctrl.c
+++ ports/stm32/powerctrl.c
@@ -33,7 +33,9 @@
 
 #if defined(STM32H5) || defined(STM32H7)
 #define RCC_SR          RSR
-#if defined(STM32H747xx)
+#if defined(STM32H743xx) || defined(STM32H750xx) || defined(STM32H753xx)
+#define RCC_SR_SFTRSTF  RCC_RSR_SFTRSTF
+#elif defined(STM32H747xx)
 #define RCC_SR_SFTRSTF  RCC_RSR_SFT2RSTF
 #else
 #define RCC_SR_SFTRSTF  RCC_RSR_SFTRSTF
